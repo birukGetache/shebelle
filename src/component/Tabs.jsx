bc17d45f271cb008b0287aeafd5bcd4ffc4365fe
birukGetachew <burab1742@gmail.com>
2024-09-03 00:03:12 -0700

    hellow

--- src/component/Tabs.jsx
+++ src/component/Tabs.jsx
@@ -15,24 +15,23 @@ const NavBar = styled.nav`
 `;
 
 const Tab = styled.button`
-margin:10px;
-  background-color: ${props => (props.active ? '#f4ca01' : '#ccc' )};
+  margin: 10px;
+  background-color: ${props => (props.active ? '#f4ca01' : '#ccc')};
   color: ${props => (props.active ? 'white' : '#f4ca01')};
-  border:  2px solid  ${props => (props.active ? '#f4ca01' : 'transparent' )};
-  padding:10px;
+  border: 2px solid ${props => (props.active ? '#f4ca01' : 'transparent')};
+  padding: 10px;
   font-size: 1rem;
-  min-width:100px;
+  min-width: 100px;
   cursor: pointer;
   font-family: "Poppins", sans-serif;
   transition: background-color 0.3s, color 0.3s;
   border-radius: 10px;
-  
+
   &:hover {
     background-color: #f4ca01;
     color: white;
   }
 `;
-
 
 const Tabs = () => {
   const activeTab = useSelector(state => state.tabs.activeTab);
